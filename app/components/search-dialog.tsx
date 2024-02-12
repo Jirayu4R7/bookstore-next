1a4b5442ab052c88a150854a785ed0cec0ae4475
Jirayu4R7 <Jirayu4R7@users.noreply.gitlab.com>
2024-02-12 17:33:24 +0700

    ⌘ + K to open dialog search.

--- app/components/search-dialog.tsx
+++ app/components/search-dialog.tsx
@@ -11,9 +11,10 @@ import {
 } from "@/app/components/ui/dialog";
 import Link from "next/link";
 import Image from "next/image";
-import { calPriceDiscount } from "@/lib/utils";
+import { calPriceDiscount, cn } from "@/lib/utils";
+import { DialogProps } from "@radix-ui/react-dialog";
 
-const SearchDialog = () => {
+const SearchDialog = ({ ...props }: DialogProps) => {
   const [open, setOpen] = useState(false);
   const [searchTerm, setSearchTerm] = useState("");
   const [result, setResult] = useState<any | null>(null);
@@ -28,7 +29,11 @@ const SearchDialog = () => {
         setResult({ data: data });
       });
     } else {
-      setResult({ data: [] });
+      if (debouncedSearchTerm === "") {
+        searchBooks("").then((data) => {
+          setResult({ data: data });
+        });
+      }
     }
   }, [debouncedSearchTerm]);
 
@@ -41,26 +46,52 @@ const SearchDialog = () => {
     setDebouncedSearchTerm("");
     setOpen((prevState) => !prevState);
   };
+
+  useEffect(() => {
+    const down = (e: KeyboardEvent) => {
+      if ((e.key === "k" && (e.metaKey || e.ctrlKey)) || e.key === "/") {
+        if (
+          (e.target instanceof HTMLElement && e.target.isContentEditable) ||
+          e.target instanceof HTMLInputElement ||
+          e.target instanceof HTMLTextAreaElement ||
+          e.target instanceof HTMLSelectElement
+        ) {
+          return;
+        }
+
+        e.preventDefault();
+        setOpen((open) => !open);
+      }
+    };
+
+    document.addEventListener("keydown", down);
+    return () => document.removeEventListener("keydown", down);
+  }, []);
   return (
     <Dialog open={open} onOpenChange={handleDialog}>
       <DialogTrigger asChild>
         <button
           type="button"
           aria-label="Search"
-          className="flex h-full items-center gap-x-2 py-1 pl-1 pr-2"
+          className="mb-1 flex h-full items-center gap-x-2 rounded-md border py-1 pl-1 pr-2"
         >
-          <SearchIcon className="stroke-skin-dark stroke-2" />
-          <span className="hidden md:inline">Search</span>
+          <SearchIcon className="stroke h-5 w-5 stroke-skin-dark" />
+          <span className="hidden text-sm font-light md:inline">
+            Search books...
+          </span>
+          <kbd className="pointer-events-none right-[0.3rem] hidden h-5 select-none items-center gap-1 rounded border bg-muted px-1.5 font-mono text-[10px] font-medium opacity-100 sm:flex">
+            <span className="text-xs">⌘</span>K
+          </kbd>
         </button>
       </DialogTrigger>
-      <DialogContentSearch className="data-[state=open]:animate-contentShow fixed border-0 top-1/4 bottom-3/4 left-1/2 z-50 max-h-[85vh] w-[90vw] max-w-[450px] -translate-x-1/2 -translate-y-1/2 rounded-md bg-noe focus:outline-none">
+      <DialogContentSearch className="data-[state=open]:animate-contentShow bg-noe fixed bottom-3/4 left-1/2 top-1/4 z-50 max-h-[85vh] w-[90vw] -translate-x-1/2 -translate-y-1/2 rounded-md border-0 focus:outline-none md:max-w-[500px]">
         <div className="flex w-full justify-between">
           <label className="relative block w-full">
             <span className="absolute inset-y-0 left-0 flex items-center pl-2 opacity-75">
               <SearchIcon />
             </span>
             <input
-              placeholder="Find books"
+              placeholder="Search books"
               type="text"
               onChange={(e) => handleInput(e)}
               className="block w-full rounded border-2 border-skin-base border-opacity-40 bg-skin-base py-3 pl-10 pr-16 caret-skin-accent placeholder:italic placeholder:text-opacity-75 focus:border-skin-accent focus:outline-none"
@@ -69,10 +100,10 @@ const SearchDialog = () => {
             <DialogClose asChild>
               <button
                 type="button"
-                className="absolute inset-y-0 right-0 my-auto hidden mr-3 h-6 w-6 appearance-none items-center justify-center rounded  border border-slate-200 px-5 text-skin-dark shadow-md hover:border-slate-300 active:bg-slate-100 md:flex"
+                className="absolute inset-y-0 right-0 my-auto mr-3 hidden h-6 w-6 appearance-none items-center justify-center rounded border border-slate-200 px-5 text-skin-dark shadow-md hover:border-slate-300 active:bg-slate-100 md:flex"
                 aria-label="Close"
               >
-                <span>Esc</span>
+                <span className="text-sm">Esc</span>
               </button>
             </DialogClose>
           </label>
@@ -88,11 +119,13 @@ const SearchDialog = () => {
           </div>
         ) : (
           <div
-            className={`h-80 rounded bg-skin-base p-2 ${
-              result ? "block" : "hidden"
-            }`}
+            className={cn(
+              "max-h-[356px] h-[348px] rounded bg-skin-base p-2",
+              result ? "block" : "hidden",
+              result?.data.length > 1 ? "h-[348px]" : "h-fit"
+            )}
           >
-            <ul className="h-full overflow-y-auto">
+            <ul className="h-full divide-y overflow-y-auto">
               {result?.data.map((book: any) => {
                 const {
                   slug,
@@ -106,7 +139,7 @@ const SearchDialog = () => {
                 return (
                   <li key={id}>
                     <Link
-                      href={`/product/${slug}`}
+                      href={`/product/${slug}?title=${title}`}
                       onClick={() => setOpen(false)}
                       className="mb-1 flex items-center gap-x-2 rounded p-2 hover:bg-skin-muted hover:bg-opacity-50"
                     >
@@ -124,7 +157,7 @@ const SearchDialog = () => {
                         />
                       </div>
                       <div className="flex w-full flex-col justify-start">
-                        <div className="h-5 text-ellipsis line-clamp-2">
+                        <div className="line-clamp-2 h-5 text-ellipsis">
                           {title}
                         </div>
                         {author ? (
@@ -134,9 +167,9 @@ const SearchDialog = () => {
                         ) : (
                           <div className="text-sm italic opacity-75">-</div>
                         )}
-                        <div className="flex md:flex-row flex-col gap-1 items-baseline">
+                        <div className="flex flex-col items-baseline gap-1 md:flex-row">
                           {discount_percent > 0 && (
-                            <div className="price text-sm line-through text-gray-600 -mb-2">
+                            <div className="price -mb-2 text-sm text-gray-600 line-through">
                               <span>{price.toLocaleString()}</span>
                               <span> บาท</span>
                             </div>
